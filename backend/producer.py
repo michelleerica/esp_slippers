7fc6148822a71e6ef2946d06a973669f4a5d4d57
Michello Lo <michelle.erica.lo@gmail.com>
2020-11-02 11:45:47 +1100

    Producer emits sensor events

--- backend/producer.py
+++ backend/producer.py
@@ -25,6 +25,7 @@
 from confluent_kafka import Producer, KafkaError
 import json
 import socket
+import websocket
 
 if __name__ == '__main__':
 
@@ -52,13 +53,17 @@ if __name__ == '__main__':
             print("Produced record to topic {} partition [{}] @ offset {}"
                   .format(msg.topic(), msg.partition(), msg.offset()))
 
-    for n in range(10):
-        record_key = "alice"
-        record_value = json.dumps({'count': n})
+    ws = websocket.WebSocket()
+    ws.connect("ws://192.168.179.20/ws")
+    print("connected")
+
+    while True:
+        result = ws.recv()
+        print(result)
+        record_key = "esp_32"
+        record_value = result
         print("Producing record: {}\t{}".format(record_key, record_value))
         producer.produce(topic, key=record_key, value=record_value, on_delivery=acked)
-        # p.poll() serves delivery reports (on_delivery)
-        # from previous produce() calls.
         producer.poll(0)
 
     producer.flush()
