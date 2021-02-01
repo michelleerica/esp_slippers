73e4022cbb528a101f019e71b28a707b79baf7c9
Michello Lo <michelle.erica.lo@gmail.com>
2021-02-01 16:47:32 +1100

    Add dat gui and get more feedback on data coming through from ESP32

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,12 +1,30 @@
 // data: x: -7.668800, y: 0.076688, z: 6.518480, compass: 208.944199
+console.log("hello")
 
 let circles = [];
 let circleIndex = 0;
 let circleCount = 0;
 const MAX_CIRCLES = 100;
 
+let sensor = { x: 0, y: 0, z: 0, compass: 0 };
+
 let drawPointer = {};
 let colorCounter = 100;
+let center = {};
+let graph = {
+    x: [],
+    y: [],
+    z: [],
+    compass: [],
+    index: 0
+}
+
+const controls = {
+    xyScale: 10,
+    x: 0, y: 0, z: 0,
+    gravity: 7,
+    clearScreen: true
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight); 
@@ -16,6 +34,20 @@ function setup() {
   frameRate(10);
   noLoop();
   drawPointer = { x: width/2,  y: height/2 };
+
+  const gui = new dat.GUI();
+  gui.add(controls, "xyScale", 10, 20);
+  gui.add(controls, "gravity", -2, 12);
+  gui.add(controls, "clearScreen");
+
+  gui.add(sensor, "x", -20, 20).listen();
+  gui.add(sensor, "y", -20, 20).listen();
+  gui.add(sensor, "z", -20, 20).listen();
+  gui.add(sensor, "compass", 0, 360).listen();
+  center = {
+      x: windowWidth/2,
+      y: windowHeight/2
+  }
 }
 
 const objectConverter = str => {
@@ -27,7 +59,7 @@ const objectConverter = str => {
         .map(_ => _.trim())
     })
     .reduce((accumulator, currentValue) => {
-      accumulator[currentValue[0]] = currentValue[1] ? Math.abs(currentValue[1]) : 0
+      accumulator[currentValue[0]] = +currentValue[1]; //? currentValue[1] : 0
       return accumulator
     }, {})
 }
@@ -48,17 +80,42 @@ function checkWrapping() {
 }
 
 function drawCircles(x, y, z) {
-    background(0); // clears screen
+    if( controls.clearScreen ) background(0); // clears screen
 
-    drawPointer.x = (x * 100) % windowWidth;
-    drawPointer.y = (y * 100) % windowHeight;
-    drawPointer.z = (z * 5) % 100;
-    checkWrapping();
+    stroke(50);
+    line(windowWidth/2, 0, windowWidth/2, windowHeight); // crosshairs
+    line(0, windowHeight/2, windowWidth, windowHeight/2); // crosshairs
+    
+
+    stroke(255, 0, 0);
+    
+    const xGraphPosition = 100;
+    
+    // for (let i = 1; i < windowWidth; i++) {
+    //     line(
+    //         i-1, xGraphPosition + map(graph.z[i-1], -10, 10, -100, 100),
+    //         i,   xGraphPosition + map(graph.z[i], -10, 10, -100, 100)
+    //     );
+    // }
+
+    noStroke();
+
+    // drawPointer.x = (x * controls.xyScale) % windowWidth;
+    // drawPointer.y = (y * controls.xyScale) % windowHeight;
+    // drawPointer.z = ( (z - GRAVITY) * 5) % 100;  // subtract 10 to remove the influence of gravity!
+    // checkWrapping();
+
+    drawPointer.x = map(y,   -controls.xyScale, controls.xyScale,  -windowWidth/2,  windowWidth/2 );
+    drawPointer.y = map(z-controls.gravity, -controls.xyScale, controls.xyScale, -windowHeight/2, windowHeight/2 );
+    drawPointer.z = map(x,   -controls.xyScale, controls.xyScale, 10, 100);
+
+    window.draw = drawPointer;
 
     circles[circleIndex] = {
         size: drawPointer.z,
         x: drawPointer.x,
         y: drawPointer.y,
+        z: drawPointer.z * 5,
         hue: colorCounter % 255,
         bright: 255,
         opacity: (colorCounter + 75) % 255
@@ -70,11 +127,17 @@ function drawCircles(x, y, z) {
       circleIndex = (circleIndex + 1) % MAX_CIRCLES;
       colorCounter++;
 
-    for (let i = 0; i < circles.length; i++) {
-      var c = circles[i];
-      fill(c.hue, 100, c.bright, c.opacity);
-      ellipse(c.x, c.y, c.size, c.size); // draws circle
-    } // for
+    // for (let i = 0; i < circles.length; i++) {
+    //   var c = circles[i];
+    //   fill(c.hue, 100, c.bright, c.opacity);
+    //   ellipse(
+    //     center.x + c.x,  // what the accelerometer calls 'y' is actually left-right horizontal movement
+    //     center.y + c.y,  // use accel up/down for screen y axis
+    //     c.size,
+    //     c.size); // draws circle
+    // } // for
+      fill(colorCounter % 255, 100, 255, (colorCounter + 75) % 255);
+      ellipse( center.x + drawPointer.x, center.y + drawPointer.y, drawPointer.z, drawPointer.z);
 } 
 
 function webSocketInvoke() {
@@ -89,7 +152,17 @@ function webSocketInvoke() {
         ws.onmessage = function (evt) { 
             const message = evt.data;
             const sensorData = objectConverter(message);
-            console.log('sensorData:', sensorData.x, sensorData.y)
+            window.sensor = sensorData;
+            sensor.x = sensorData.x;
+            sensor.y = sensorData.y;
+            sensor.z = sensorData.z - controls.gravity;
+            sensor.compass = sensorData.compass;
+            // console.log('sensorData:', sensorData.x, sensorData.y)
+            graph.x[graph.index] = sensor.x;
+            graph.y[graph.index] = sensor.y;
+            graph.z[graph.index] = sensor.z;
+            graph.compass[graph.index] = sensor.compass;
+            graph.index = (graph.index + 1 ) % windowWidth;
             drawCircles(sensorData.x, sensorData.y, sensorData.z);
         };
         
@@ -102,3 +175,12 @@ function webSocketInvoke() {
  }
  webSocketInvoke();
 
+function keyPressed(key){
+    // console.log(key);
+
+    if( key.key === ' ' ){
+        background( 0 );
+        circles.length = 0;
+        circleIndex = 0;
+    }
+} // keyPressed
